635b4030046d41b21a43d57d8cacf06918c80570
chafikkhaoula <khchafik99@gmail.com>
2023-05-06 12:04:20 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,25 +2,24 @@
 
 /**
  * print_sign - Prints the sign of a number
- * @c: The number to be checked
+ * @n: The number to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 int print_sign(int n)
 {
-
 if (n > 0)
 {
 _putchar(43);
-return(1);
+return (1);
 }
-else if (n <0)
+else if (n < 0)
 {
 _putchar(45);
-return(-1);
+return (-1);
 }
 else
 {
 _putchar(48);
-return(0);
+return (0);
 }
 }
