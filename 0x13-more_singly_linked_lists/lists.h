f5762878b17a15be11da32cef69c26ae32729c90
chafikkhaoula <khchafik99@gmail.com>
2023-08-01 05:44:31 -0700

    up

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -41,5 +41,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 /*prototype function that inserts a new node at a given position.*/
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 /*prototy pefunction that deletes the node at index index of a listint_t.*/
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LISTS_H */
